bcfa0db80851a56556aceeb97febe8dfd52f28b5
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-04 14:29:23 -0300

    feat[cards]: increase logic to cards service

--- cards/src/main/java/com/example/cards/service/CardsServiceImpl.java
+++ cards/src/main/java/com/example/cards/service/CardsServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.cards.service;
 
 import com.example.cards.constants.CardsConstants;
+import com.example.cards.dto.CardsDTO;
 import com.example.cards.entity.Cards;
+import com.example.cards.exception.CardAlreadyExistsException;
+import com.example.cards.exception.ResourceNotFoundException;
+import com.example.cards.mapper.CardsMapper;
 import com.example.cards.repository.CardsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -15,9 +20,38 @@ public class CardsServiceImpl implements ICardsService{
     private CardsRepository cardsRepository;
     @Override
     public void createCard(String mobileNumber) {
-        if (cardsRepository.findByMobileNumber(mobileNumber).isEmpty()){
-            cardsRepository.save(createNewCard(mobileNumber));
+        Optional<Cards> cards = cardsRepository.findByMobileNumber(mobileNumber);
+        if (cards.isPresent()){
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber" + mobileNumber);
         }
+        cardsRepository.save(createNewCard(mobileNumber));
+    }
+
+    @Override
+    public CardsDTO fetchCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(()-> new ResourceNotFoundException("Card","mobileNumber",mobileNumber));
+
+        return CardsMapper.mapToCardsDTO(cards,new CardsDTO());
+    }
+
+    @Override
+    public boolean updateCard(CardsDTO cardsDTO) {
+        Cards cards = cardsRepository.findByMobileNumber(cardsDTO.getMobileNumber())
+                .orElseThrow(()-> new ResourceNotFoundException("Card", "cardNumber", cardsDTO.getCardNumber()));
+
+        CardsMapper.mapToCards(cardsDTO,cards);
+        cardsRepository.save(cards);
+        return true;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(()-> new ResourceNotFoundException("Card","mobileNumber",mobileNumber));
+
+        cardsRepository.deleteById(cards.getCardId());
+        return true;
     }
 
     private Cards createNewCard(String mobileNumber){
@@ -30,10 +64,5 @@ public class CardsServiceImpl implements ICardsService{
         card.setAmountUsed(0);
         card.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
         return card;
-    }
-
-    @Override
-    public boolean deleteCard(String mobileNumber) {
-        return false;
     }
 }
--- cards/src/main/java/com/example/cards/service/ICardsService.java
+++ cards/src/main/java/com/example/cards/service/ICardsService.java
@@ -1,8 +1,12 @@
 package com.example.cards.service;
 
+import com.example.cards.dto.CardsDTO;
+
 public interface ICardsService {
 
     void createCard(String mobileNumber);
+    CardsDTO fetchCard(String mobileNumber);
+    boolean updateCard(CardsDTO cardsDTO);
     boolean deleteCard(String mobileNumber);
 
 }
