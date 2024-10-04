f2ba8480a39af7ffb839ab36f133d6c66b0d0df2
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-04 01:27:23 -0300

    feat[cards]: create dto and mapper to card entity

---
+++ cards/src/main/java/com/example/cards/mapper/CardsMapper.java
@@ -0,0 +1,26 @@
+package com.example.cards.mapper;
+
+import com.example.cards.dto.CardsDTO;
+import com.example.cards.entity.Cards;
+
+public class CardsMapper {
+    public static CardsDTO mapToCardsDTO(Cards cards, CardsDTO cardsDTO){
+        cardsDTO.setCardNumber(cards.getCardNumber());
+        cardsDTO.setCardType(cards.getCardType());
+        cardsDTO.setMobileNumber(cards.getMobileNumber());
+        cardsDTO.setTotalLimit(cards.getTotalLimit());
+        cardsDTO.setAmountUsed(cards.getAmountUsed());
+        cardsDTO.setAvailableAmount(cards.getAvailableAmount());
+        return cardsDTO;
+    }
+
+    public static Cards mapToCards(CardsDTO cardsDTO, Cards cards){
+        cards.setCardNumber(cardsDTO.getCardNumber());
+        cards.setCardType(cardsDTO.getCardType());
+        cards.setMobileNumber(cardsDTO.getMobileNumber());
+        cards.setTotalLimit(cardsDTO.getTotalLimit());
+        cards.setAmountUsed(cardsDTO.getAmountUsed());
+        cards.setAvailableAmount(cardsDTO.getAvailableAmount());
+        return cards;
+    }
+}
