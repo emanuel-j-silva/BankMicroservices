8e5e30537f32da63a17e96986f98b16151c6bcc2
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-05 00:46:57 -0300

    feat[loans]: implement global exception handler and response dtos

---
+++ loans/src/main/java/com/example/loans/dto/ResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.loans.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class ResponseDTO {
+    private String statusCode;
+    private String statusMsg;
+}
