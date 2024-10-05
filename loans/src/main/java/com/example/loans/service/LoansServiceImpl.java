fae2fde756cf55d3a2ffa68f5e16a6a25e3507fd
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-10-05 00:22:10 -0300

    feat[loans]: implement create loans service, create exceptions and constant values

--- loans/src/main/java/com/example/loans/service/LoansServiceImpl.java
+++ loans/src/main/java/com/example/loans/service/LoansServiceImpl.java
@@ -1,11 +1,41 @@
 package com.example.loans.service;
 
+import com.example.loans.constants.LoansConstants;
 import com.example.loans.dto.LoansDTO;
+import com.example.loans.entity.Loans;
+import com.example.loans.exception.LoanAlreadyExistsException;
+import com.example.loans.repository.LoansRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
 public class LoansServiceImpl implements ILoansService{
+    private LoansRepository loansRepository;
+
     @Override
     public void createLoan(String mobileNumber) {
+        Optional<Loans> loans = loansRepository.findByMobileNumber(mobileNumber);
+        if (loans.isPresent()){
+            throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber " + mobileNumber);
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
 
+    private Loans createNewLoan(String mobileNumber){
+        Loans loan = new Loans();
+
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        loan.setLoanNumber(Long.toString(randomLoanNumber));
+        loan.setMobileNumber(mobileNumber);
+        loan.setLoanType(LoansConstants.HOME_LOAN);
+        loan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        loan.setAmountPaid(0);
+        loan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return loan;
     }
 
     @Override
